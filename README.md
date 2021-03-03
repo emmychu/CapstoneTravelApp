79a6fd758cca1a99bf028dad4fe37e059039a93b
Emma <lin.chu@Utexas.edu>
2021-03-03 17:44:09 -0600

    updated html, installed packages

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 * `npm i express`
 * `npm i cors`
 * `npm i body-parser`
+* `npm install -D webpack`
+* `npm i webpack-cli`
+* `npm i -D @babel/core @babel/preset-env babel-loader`
+* `npm i -D style-loader node-sass css-loader sass-loader`
+* `npm i -D clean-webpack-plugin`
+* `npm i -D html-webpack-plugin`
+* `npm i -D mini-css-extract-plugin`
+* `npm i -D optimize-css-assets-webpack-plugin terser-webpack-plugin`
---
+++ src/server/index.js
@@ -0,0 +1,22 @@
+var path = require('path')
+const express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.listen(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use(express.static('dist'))
+console.log(__dirname)
+
+app.get('/', (request,response) => {
+    response.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+app.listen(8090, () => {
+    console.log('Travel App listening on port 8090!')
+})
