df7525be4f2e1e83970ba5ae1538c681819ac808
Emma <lin.chu@Utexas.edu>
2021-06-03 23:44:10 -0500

    updates to js to change UI

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,15 @@
 import './styles/base.scss'
 import './styles/step1.scss'
 import './styles/step2.scss'
+
+import {uiUpdate} from './js/changeUI'
+import {createCalendar} from './js/createCalendar'
+// import {dateCall} from './js/dataCall'
+
+
+export {
+    uiUpdate,
+    // createCalendar,
+    // dateCall
+}
+
--- src/client/js/changeUI.js
+++ src/client/js/changeUI.js
@@ -1,22 +1,10 @@
 // Should update UI from step 1 to step 2
-submitButton = document.getElementById("submit-button")
-submitButton.addEventListener("click", uiUpdate())
+let submitButton = document.getElementById("submit-button")
+submitButton.addEventListener("click", uiUpdate());
 
-function uiUpdate(){
-    let multiCity = false
+function uiUpdate(destination,multiCity,departDate,returnDate){
     document.getElementById("step-1").style.display = "none";
     document.getElementById("step-2").style.display = "flex";
-    document.getElementById("destination") = document.getElementById("first-destination").value
-    let departDate = new Date()
-    let returnDate = new Date()
-    departDate = document.getElementById("depart-date").value
-    returnDate = document.getElementById("return-date").value
-    let departMonth = parseInt(departDate.slice(5,6))
-    let returnMonth = parseInt(returnDate.slice(5,6))
-    let duration = (returnDate.getTime() - departDate.getTime)/(1000 * 60 * 60 *24)
-    if(document.getElementById("multicity").checked){
-        multiCity = true;
-    }
     createCalendar(duration,multiCity,departMonth,returnMonth)
 }
 
